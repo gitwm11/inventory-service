2e5cde40bd059ef3468fefa3503a63331a4b34c9
ElMehdi WADDAD <wadadmehdi11@gmail.com>
2024-04-01 21:58:49 +0000

    entities

---
+++ src/main/java/ma/xproce/inventoryservice/dao/entities/Creator.java
@@ -0,0 +1,7 @@
+package ma.xproce.inventoryservice.dao.entities;
+
+public class Creator {
+    private Integer id;
+    private String name;
+    private String email;
+}
---
+++ src/main/java/ma/xproce/inventoryservice/dao/entities/Video.java
@@ -0,0 +1,12 @@
+package ma.xproce.inventoryservice.dao.entities;
+
+import java.util.Date;
+
+public class Video {
+    private Integer id;
+    private String name;
+    private String url;
+    private String description;
+    private Date datePublication;
+
+}
