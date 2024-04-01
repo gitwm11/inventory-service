63646bb7e7c14654bda79563ad86d14432824fad
ElMehdi WADDAD <wadadmehdi11@gmail.com>
2024-04-01 22:18:50 +0000

    annotations

--- src/main/java/ma/xproce/inventoryservice/dao/entities/Creator.java
+++ src/main/java/ma/xproce/inventoryservice/dao/entities/Creator.java
@@ -1,7 +1,22 @@
 package ma.xproce.inventoryservice.dao.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "videos")
 public class Creator {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
+    @OneToMany(mappedBy = "creator",fetch = FetchType.LAZY)
+    private Collection<Video> videos;
 }
--- src/main/java/ma/xproce/inventoryservice/dao/entities/Video.java
+++ src/main/java/ma/xproce/inventoryservice/dao/entities/Video.java
@@ -1,12 +1,26 @@
 package ma.xproce.inventoryservice.dao.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "creators")
 public class Video {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String url;
     private String description;
     private Date datePublication;
+
+    @ManyToOne
+    private Creator creator;
 
 }
